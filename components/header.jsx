f7425ff148391a67ac8a635375dedc92ce49be4e
Mahmoud Wael Hamdy <mhmwh38@gmail.com>
2025-12-27 22:07:08 +0200

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -5,13 +5,10 @@ import githubLogo from 'public/images/github-mark-white.svg';
 
 const navItems = [
     { linkText: 'Home', href: '/' },
-    { linkText: 'Revalidation', href: '/revalidation' },
-    { linkText: 'Image CDN', href: '/image-cdn' },
-    { linkText: 'Edge Function', href: '/edge' },
-    { linkText: 'Blobs', href: '/blobs' },
-    { linkText: 'Classics', href: '/classics' },
-    { linkText: 'Middleware', href: '/middleware' },
-    { linkText: 'Routing', href: '/routing' }
+    { linkText: 'projects', href: '/revalidation' },
+    { linkText: 'Courses', href: '/image-cdn' },
+    { linkText: 'Exprience', href: '/edge' },
+    
 ];
 
 export function Header() {
